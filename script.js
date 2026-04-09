c484683baa5ac69a9bc94ccee1a347fc0f7fb2bd
learner-trying <the77358@gmail.com>
2026-04-09 19:55:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function change(){
+let a=
+document.getElementById("number").value;
+
+
+if(a=="27-04-2004"){
+ document.getElementById("result").innerText="happy birthday to you";
+
+}
+else if(a=="31-07-2005"){
+ document.getElementById("result").innerText="happy birthday to Boss";   
+}
+else{
+document.getElementById("result").innerText="today is not your birthday ";    
+}
+
+}
