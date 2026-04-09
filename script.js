1392bb63f220af26428903604e0fbfaf37700b29
learner-trying <the77358@gmail.com>
2026-04-09 20:48:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ if(a=="27-04-2004"){
 else if(a=="31-07-2005"){
  document.getElementById("result").innerText="happy birthday to Boss";   
 }
+ else if(a=="1975"){
+  document.getElementById("result").innerText="janam din mubarak ho ammi,love you";
 else{
 document.getElementById("result").innerText="today is not your birthday ";    
 }
