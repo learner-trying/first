dec5bd380c75c790717c8b0c52c0e9966e4d39f0
yeah <the77358@gmail.com>
2026-04-11 12:13:08 +0530

    test

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ else if(a=="31-07-2005"){
  document.getElementById("result").innerText="happy birthday to Boss";   
 }
 else if(a=="1975"){
-document.getElementById("result").innerText="janam din mubarak ho";    
+document.getElementById("result").innerText="janam din mubarak ho ammi,love you";    
 }
 else{
 document.getElementById("result").innerText="today is not your birthday ";    
