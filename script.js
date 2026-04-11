fd99e757b57beee98c382ec8a3eb384a329b3568
learner-trying <the77358@gmail.com>
2026-04-11 19:22:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ else if(a=="31-07-2005"){
 else if(a=="1975"){
 document.getElementById("result").innerText="janam din mubarak ho ammi,love you";    
 }
+ else if(a=="11-09-2023"){
+document.getElementById("result").innerText="happy birthday orhu";   
+}
+else if(a=="30-10-2024"){
+ document.getElementById("result").innerText="happy birthday aizu";   
+}
+
+
+
+
+ 
 else{
 document.getElementById("result").innerText="today is not your birthday ";    
 }
