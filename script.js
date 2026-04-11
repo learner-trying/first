8bb4b2d51f409e00df14f180e88deb4786cae20a
yeah <the77358@gmail.com>
2026-04-11 12:01:11 +0530

    update

--- script.js
+++ script.js
@@ -12,27 +12,8 @@ else if(a=="31-07-2005"){
  document.getElementById("result").innerText="happy birthday to Boss";   
 }
 else if(a=="1975"){
-document.getElementById("result").innerText="janam din mubarak ho ammi,love you";    
+document.getElementById("result").innerText="janam din mubarak ho";    
 }
-else{
-document.getElementById("result").innerText="today is not your birthday ";    
-}
-
-}
-function change(){
-let a=
-document.getElementById("number").value;
-
-
-if(a=="27-04-2004"){
- document.getElementById("result").innerText="happy birthday to you";
-
-}
-else if(a=="31-07-2005"){
- document.getElementById("result").innerText="happy birthday to Boss";   
-}
- else if(a=="1975"){
-  document.getElementById("result").innerText="janam din mubarak ho ammi,love you";
 else{
 document.getElementById("result").innerText="today is not your birthday ";    
 }
