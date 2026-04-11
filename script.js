b88f126dbf8893e30eb5ce0330f9fa46f8d86208
learner-trying <the77358@gmail.com>
2026-04-11 11:52:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,22 +19,3 @@ document.getElementById("result").innerText="today is not your birthday ";
 }
 
 }
-function change(){
-let a=
-document.getElementById("number").value;
-
-
-if(a=="27-04-2004"){
- document.getElementById("result").innerText="happy birthday to you";
-
-}
-else if(a=="31-07-2005"){
- document.getElementById("result").innerText="happy birthday to Boss";   
-}
- else if(a=="1975"){
-  document.getElementById("result").innerText="janam din mubarak ho ammi,love you";
-else{
-document.getElementById("result").innerText="today is not your birthday ";    
-}
-
-}
